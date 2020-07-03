22743c3192ccb72dc0b1c4e2eeae03f1528cb7f0
reyzaldy <Reyzaldy Indra>
2020-07-03 14:05:00 +0700

    add sample api responses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form'
-import Api from './Api'
+import Api from './Api/Api'
 
 class App extends React.Component {
   state = {
