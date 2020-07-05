275c84151879de61e447e69b0b37c3360a2836fe
reyzaldy <Reyzaldy Indra>
2020-07-05 09:33:44 +0700

    add bootstrap scss

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss'
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
 
